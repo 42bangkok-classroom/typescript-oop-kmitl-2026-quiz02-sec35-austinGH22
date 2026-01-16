d5457843415e4bca7586456c3ccd8e1e6598b931
PhanuphongKuharungkitcharoen <Phanuphongkuharungkitcharoen@gmail.com>
2026-01-16 10:49:39 +0700

    po4

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,1 +1,29 @@
-export function countCommentsByPost() {}
+import axios from 'axios';
+
+
+interface Comment {
+  postId: number | null | undefined;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+interface CommentCount {
+  [key: number]: number;
+}
+export const countCommentsByPost = async (): Promise<CommentCount> => {
+  try {
+    const response = await axios.get<Comment[]>("https://jsonplaceholder.typicode.com/comments");
+    const comments = response.data;
+    const result = comments.reduce((acc, curr) => {
+      const id = curr.postId; 
+      acc[id] = (acc[id] || 0) + 1;
+      return acc;
+    }, {} as CommentCount);
+
+    return result;
+
+  } catch (error) {
+    throw new Error("Failed to fetch comments");
+  }
+};
