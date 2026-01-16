8cf0ac4e52a245974d1a5964df944a0221eaf146
PhanuphongKuharungkitcharoen <Phanuphongkuharungkitcharoen@gmail.com>
2026-01-16 10:12:05 +0700

    p02

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,32 @@
-export function getPostsByUser() {}
+import axios from "axios";
+
+interface Post {
+    id: number;
+    title: string;
+}
+
+interface ApiPost {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+export async function getPostsByUser(userId: number): Promise<Post[]> {
+    try {
+        const response = await axios.get<ApiPost[]>(
+            "https://jsonplaceholder.typicode.com/posts"
+        );
+
+        const posts = response.data;
+
+        return posts
+            .filter((post: ApiPost) => post.userId === userId)
+            .map((post: ApiPost) => ({
+                id: post.id,
+                title: post.title
+            }));
+    } catch (error) {
+        throw error;
+    }
+}
